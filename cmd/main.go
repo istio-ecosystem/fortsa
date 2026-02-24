b742cdef2da58187c6efb0514f3a3dfde3761d9f
Steve Scaffidi <sscaffidi@cloudera.com>
2026-02-24 15:06:10 -0500

    copied files from fortsa2 and modified the package for imports
    Signed-off-by: Steve Scaffidi <sscaffidi@cloudera.com>

--- cmd/main.go
+++ cmd/main.go
@@ -1,233 +1,127 @@
-/*
-Copyright 2026.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package main
 
 import (
-	"crypto/tls"
+	"context"
 	"flag"
 	"os"
-	"path/filepath"
+	"strings"
+	"time"
 
-	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
-	// to ensure that exec-entrypoint and run can make use of them.
-	_ "k8s.io/client-go/plugin/pkg/client/auth"
-
+	"github.com/spf13/pflag"
+	admissionregv1 "k8s.io/api/admissionregistration/v1"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/certwatcher"
-	"sigs.k8s.io/controller-runtime/pkg/healthz"
+	"sigs.k8s.io/controller-runtime/pkg/builder"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
-	"sigs.k8s.io/controller-runtime/pkg/metrics/filters"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
-	"sigs.k8s.io/controller-runtime/pkg/webhook"
-	// +kubebuilder:scaffold:imports
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	"github.com/istio-ecosystem/fortsa/internal/controller"
+	"github.com/istio-ecosystem/fortsa/internal/webhook"
 )
 
 var (
-	scheme   = runtime.NewScheme()
-	setupLog = ctrl.Log.WithName("setup")
+	scheme = runtime.NewScheme()
 )
+
+// parseSkipNamespaces splits a comma-separated string into non-empty trimmed namespace names.
+func parseSkipNamespaces(s string) []string {
+	if s == "" {
+		return nil
+	}
+	parts := strings.Split(s, ",")
+	var out []string
+	for _, p := range parts {
+		if trimmed := strings.TrimSpace(p); trimmed != "" {
+			out = append(out, trimmed)
+		}
+	}
+	return out
+}
 
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
-
-	// +kubebuilder:scaffold:scheme
+	utilruntime.Must(corev1.AddToScheme(scheme))
 }
 
-// nolint:gocyclo
 func main() {
 	var metricsAddr string
-	var metricsCertPath, metricsCertName, metricsCertKey string
-	var webhookCertPath, webhookCertName, webhookCertKey string
 	var enableLeaderElection bool
-	var probeAddr string
-	var secureMetrics bool
-	var enableHTTP2 bool
-	var tlsOpts []func(*tls.Config)
-	flag.StringVar(&metricsAddr, "metrics-bind-address", "0", "The address the metrics endpoint binds to. "+
-		"Use :8443 for HTTPS or :8080 for HTTP, or leave as 0 to disable the metrics service.")
-	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
-	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
-		"Enable leader election for controller manager. "+
-			"Enabling this will ensure there is only one active controller manager.")
-	flag.BoolVar(&secureMetrics, "metrics-secure", true,
-		"If set, the metrics endpoint is served securely via HTTPS. Use --metrics-secure=false to use HTTP instead.")
-	flag.StringVar(&webhookCertPath, "webhook-cert-path", "", "The directory that contains the webhook certificate.")
-	flag.StringVar(&webhookCertName, "webhook-cert-name", "tls.crt", "The name of the webhook certificate file.")
-	flag.StringVar(&webhookCertKey, "webhook-cert-key", "tls.key", "The name of the webhook key file.")
-	flag.StringVar(&metricsCertPath, "metrics-cert-path", "",
-		"The directory that contains the metrics server certificate.")
-	flag.StringVar(&metricsCertName, "metrics-cert-name", "tls.crt", "The name of the metrics server certificate file.")
-	flag.StringVar(&metricsCertKey, "metrics-cert-key", "tls.key", "The name of the metrics server key file.")
-	flag.BoolVar(&enableHTTP2, "enable-http2", false,
-		"If set, HTTP/2 will be enabled for the metrics and webhook servers")
-	opts := zap.Options{
-		Development: true,
-	}
-	opts.BindFlags(flag.CommandLine)
-	flag.Parse()
+	var dryRun bool
+	var compareHub bool
+	var restartDelay time.Duration
+	var istiodConfigReadDelay time.Duration
+	var reconcilePeriod time.Duration
+	var skipNamespaces string
+	pflag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
+	pflag.BoolVar(&enableLeaderElection, "leader-elect", true, "Enable leader election for controller manager.")
+	pflag.BoolVar(&dryRun, "dry-run", false, "If true, log what would be done without annotating workloads.")
+	pflag.BoolVar(&compareHub, "compare-hub", false, "If true, require container image registry to match ConfigMap hub when detecting outdated pods.")
+	pflag.DurationVar(&restartDelay, "restart-delay", 0, "Delay between restarting each workload (e.g. 5s). Use 0 for no delay.")
+	pflag.DurationVar(&istiodConfigReadDelay, "istiod-config-read-delay", 10*time.Second, "Wait for Istiod to read the updated ConfigMap before scanning (e.g. 10s). Use 0 to skip.")
+	pflag.DurationVar(&reconcilePeriod, "reconcile-period", 1*time.Hour, "Period between full reconciliations of all istio-sidecar-injector ConfigMaps. Use 0 to disable periodic reconciliation.")
+	pflag.StringVar(&skipNamespaces, "skip-namespaces", "kube-system,istio-system", "Comma-separated list of namespaces to skip when scanning pods for outdated sidecars.")
 
-	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
+	zapOpts := zap.Options{}
+	zapOpts.BindFlags(flag.CommandLine)
+	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
+	pflag.Parse()
 
-	// if the enable-http2 flag is false (the default), http/2 should be disabled
-	// due to its vulnerabilities. More specifically, disabling http/2 will
-	// prevent from being vulnerable to the HTTP/2 Stream Cancellation and
-	// Rapid Reset CVEs. For more information see:
-	// - https://github.com/advisories/GHSA-qppj-fm5r-hxr3
-	// - https://github.com/advisories/GHSA-4374-p667-p6c8
-	disableHTTP2 := func(c *tls.Config) {
-		setupLog.Info("disabling http/2")
-		c.NextProtos = []string{"http/1.1"}
-	}
-
-	if !enableHTTP2 {
-		tlsOpts = append(tlsOpts, disableHTTP2)
-	}
-
-	// Create watchers for metrics and webhooks certificates
-	var metricsCertWatcher, webhookCertWatcher *certwatcher.CertWatcher
-
-	// Initial webhook TLS options
-	webhookTLSOpts := tlsOpts
-
-	if len(webhookCertPath) > 0 {
-		setupLog.Info("Initializing webhook certificate watcher using provided certificates",
-			"webhook-cert-path", webhookCertPath, "webhook-cert-name", webhookCertName, "webhook-cert-key", webhookCertKey)
-
-		var err error
-		webhookCertWatcher, err = certwatcher.New(
-			filepath.Join(webhookCertPath, webhookCertName),
-			filepath.Join(webhookCertPath, webhookCertKey),
-		)
-		if err != nil {
-			setupLog.Error(err, "Failed to initialize webhook certificate watcher")
-			os.Exit(1)
-		}
-
-		webhookTLSOpts = append(webhookTLSOpts, func(config *tls.Config) {
-			config.GetCertificate = webhookCertWatcher.GetCertificate
-		})
-	}
-
-	webhookServer := webhook.NewServer(webhook.Options{
-		TLSOpts: webhookTLSOpts,
-	})
-
-	// Metrics endpoint is enabled in 'config/default/kustomization.yaml'. The Metrics options configure the server.
-	// More info:
-	// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.21.0/pkg/metrics/server
-	// - https://book.kubebuilder.io/reference/metrics.html
-	metricsServerOptions := metricsserver.Options{
-		BindAddress:   metricsAddr,
-		SecureServing: secureMetrics,
-		TLSOpts:       tlsOpts,
-	}
-
-	if secureMetrics {
-		// FilterProvider is used to protect the metrics endpoint with authn/authz.
-		// These configurations ensure that only authorized users and service accounts
-		// can access the metrics endpoint. The RBAC are configured in 'config/rbac/kustomization.yaml'. More info:
-		// https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.21.0/pkg/metrics/filters#WithAuthenticationAndAuthorization
-		metricsServerOptions.FilterProvider = filters.WithAuthenticationAndAuthorization
-	}
-
-	// If the certificate is not specified, controller-runtime will automatically
-	// generate self-signed certificates for the metrics server. While convenient for development and testing,
-	// this setup is not recommended for production.
-	//
-	// TODO(user): If you enable certManager, uncomment the following lines:
-	// - [METRICS-WITH-CERTS] at config/default/kustomization.yaml to generate and use certificates
-	// managed by cert-manager for the metrics server.
-	// - [PROMETHEUS-WITH-CERTS] at config/prometheus/kustomization.yaml for TLS certification.
-	if len(metricsCertPath) > 0 {
-		setupLog.Info("Initializing metrics certificate watcher using provided certificates",
-			"metrics-cert-path", metricsCertPath, "metrics-cert-name", metricsCertName, "metrics-cert-key", metricsCertKey)
-
-		var err error
-		metricsCertWatcher, err = certwatcher.New(
-			filepath.Join(metricsCertPath, metricsCertName),
-			filepath.Join(metricsCertPath, metricsCertKey),
-		)
-		if err != nil {
-			setupLog.Error(err, "to initialize metrics certificate watcher", "error", err)
-			os.Exit(1)
-		}
-
-		metricsServerOptions.TLSOpts = append(metricsServerOptions.TLSOpts, func(config *tls.Config) {
-			config.GetCertificate = metricsCertWatcher.GetCertificate
-		})
-	}
+	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&zapOpts)))
 
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-		Scheme:                 scheme,
-		Metrics:                metricsServerOptions,
-		WebhookServer:          webhookServer,
-		HealthProbeBindAddress: probeAddr,
-		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "71f32f9d.fortsa.scaffidi.net",
-		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
-		// when the Manager ends. This requires the binary to immediately end when the
-		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
-		// speeds up voluntary leader transitions as the new leader don't have to wait
-		// LeaseDuration time first.
-		//
-		// In the default scaffold provided, the program ends immediately after
-		// the manager stops, so would be fine to enable this option. However,
-		// if you are doing or is intended to do any operation such as perform cleanups
-		// after the manager stops then its usage might be unsafe.
-		// LeaderElectionReleaseOnCancel: true,
+		Scheme:           scheme,
+		Metrics:          metricsserver.Options{BindAddress: metricsAddr},
+		LeaderElection:   enableLeaderElection,
+		LeaderElectionID: "fortsa2.scaffidi.net",
 	})
 	if err != nil {
-		setupLog.Error(err, "unable to start manager")
+		ctrl.Log.WithName("setup").Error(err, "unable to start manager")
 		os.Exit(1)
 	}
 
-	// +kubebuilder:scaffold:builder
+	webhookClient := webhook.NewWebhookClient(mgr.GetClient())
+	reconciler := controller.NewConfigMapReconciler(mgr.GetClient(), mgr.GetScheme(), dryRun, compareHub, restartDelay, istiodConfigReadDelay, parseSkipNamespaces(skipNamespaces), webhookClient)
 
-	if metricsCertWatcher != nil {
-		setupLog.Info("Adding metrics certificate watcher to manager")
-		if err := mgr.Add(metricsCertWatcher); err != nil {
-			setupLog.Error(err, "unable to add metrics certificate watcher to manager")
-			os.Exit(1)
-		}
+	builder := ctrl.NewControllerManagedBy(mgr).
+		For(&corev1.ConfigMap{}).
+		WithEventFilter(predicate.NewPredicateFuncs(controller.ConfigMapFilter())).
+		Watches(
+			&admissionregv1.MutatingWebhookConfiguration{},
+			handler.EnqueueRequestsFromMapFunc(func(_ context.Context, obj client.Object) []reconcile.Request {
+				return []reconcile.Request{controller.PeriodicReconcileRequest()}
+			}),
+			builder.WithPredicates(predicate.NewPredicateFuncs(controller.MutatingWebhookFilter())),
+		).
+		Watches(
+			&corev1.Namespace{},
+			handler.EnqueueRequestsFromMapFunc(func(_ context.Context, obj client.Object) []reconcile.Request {
+				return []reconcile.Request{controller.NamespaceReconcileRequest(obj.GetName())}
+			}),
+			builder.WithPredicates(controller.NamespaceFilter()),
+		)
+	if reconcilePeriod > 0 {
+		builder = builder.WatchesRawSource(controller.NewPeriodicReconcileSource(reconcilePeriod))
 	}
-
-	if webhookCertWatcher != nil {
-		setupLog.Info("Adding webhook certificate watcher to manager")
-		if err := mgr.Add(webhookCertWatcher); err != nil {
-			setupLog.Error(err, "unable to add webhook certificate watcher to manager")
-			os.Exit(1)
-		}
-	}
-
-	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
-		setupLog.Error(err, "unable to set up health check")
+	err = builder.Complete(reconciler)
+	if err != nil {
+		ctrl.Log.WithName("setup").Error(err, "unable to create controller")
 		os.Exit(1)
 	}
-	if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
-		setupLog.Error(err, "unable to set up ready check")
-		os.Exit(1)
-	}
 
-	setupLog.Info("starting manager")
+	if dryRun {
+		ctrl.Log.WithName("setup").Info("starting manager in dry-run mode (no workloads will be annotated)")
+	} else {
+		ctrl.Log.WithName("setup").Info("starting manager")
+	}
 	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
-		setupLog.Error(err, "problem running manager")
+		ctrl.Log.WithName("setup").Error(err, "problem running manager")
 		os.Exit(1)
 	}
 }
---
+++ internal/controller/configmap_reconciler_test.go
@@ -0,0 +1,334 @@
+package controller
+
+import (
+	"context"
+	"sync"
+	"testing"
+	"time"
+
+	admissionregv1 "k8s.io/api/admissionregistration/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+
+	"github.com/istio-ecosystem/fortsa/internal/podscanner"
+)
+
+// countingAnnotator counts Annotate calls for testing.
+type countingAnnotator struct {
+	mu    sync.Mutex
+	count int
+}
+
+func (c *countingAnnotator) Annotate(_ context.Context, _ podscanner.WorkloadRef) error {
+	c.mu.Lock()
+	c.count++
+	c.mu.Unlock()
+	return nil
+}
+
+func (c *countingAnnotator) getCount() int {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return c.count
+}
+
+func TestFetchTagToRevision(t *testing.T) {
+	scheme := runtime.NewScheme()
+	_ = admissionregv1.AddToScheme(scheme)
+	_ = corev1.AddToScheme(scheme)
+
+	mwc := &admissionregv1.MutatingWebhookConfiguration{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "istio-revision-tag-canary",
+			Labels: map[string]string{
+				"istio.io/tag": "canary",
+				"istio.io/rev": "1-20",
+			},
+		},
+	}
+	fakeClient := fake.NewClientBuilder().WithScheme(scheme).WithObjects(mwc).Build()
+
+	got, err := fetchTagToRevision(context.Background(), fakeClient)
+	if err != nil {
+		t.Fatalf("fetchTagToRevision: %v", err)
+	}
+	if got["canary"] != "1-20" {
+		t.Errorf("fetchTagToRevision: want canary->1-20, got %v", got)
+	}
+}
+
+func TestPeriodicReconcileRequest(t *testing.T) {
+	req := PeriodicReconcileRequest()
+	if req.Namespace != "istio-system" {
+		t.Errorf("PeriodicReconcileRequest namespace = %q, want istio-system", req.Namespace)
+	}
+	if req.Name != "__periodic_reconcile__" {
+		t.Errorf("PeriodicReconcileRequest name = %q, want __periodic_reconcile__", req.Name)
+	}
+}
+
+func TestConfigMapReconciler_Reconcile_PeriodicTrigger(t *testing.T) {
+	scheme := runtime.NewScheme()
+	_ = admissionregv1.AddToScheme(scheme)
+	_ = corev1.AddToScheme(scheme)
+	cm := &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{Namespace: "istio-system", Name: "istio-sidecar-injector"},
+		Data: map[string]string{
+			"values": `{"revision":"default","global":{"hub":"docker.io/istio","tag":"1.20.1","proxy":{"image":"proxyv2"}}}`,
+		},
+	}
+	fakeClient := fake.NewClientBuilder().WithScheme(scheme).WithObjects(cm).Build()
+	r := NewConfigMapReconciler(fakeClient, scheme, false, true, 0, 0, nil, nil)
+	req := PeriodicReconcileRequest()
+	_, err := r.Reconcile(context.Background(), req)
+	if err != nil {
+		t.Fatalf("Reconcile(periodic): %v", err)
+	}
+	// Periodic reconcile runs scan; with nil webhook, scanner returns no workloads
+	// Verifies reconcileAll runs without error
+}
+
+func TestMutatingWebhookFilter(t *testing.T) {
+	filter := MutatingWebhookFilter()
+	tests := []struct {
+		name     string
+		obj      *admissionregv1.MutatingWebhookConfiguration
+		wantPass bool
+	}{
+		{
+			name: "istio-revision-tag-default",
+			obj: &admissionregv1.MutatingWebhookConfiguration{
+				ObjectMeta: metav1.ObjectMeta{Name: "istio-revision-tag-default"},
+			},
+			wantPass: true,
+		},
+		{
+			name: "istio-revision-tag-canary",
+			obj: &admissionregv1.MutatingWebhookConfiguration{
+				ObjectMeta: metav1.ObjectMeta{Name: "istio-revision-tag-canary"},
+			},
+			wantPass: true,
+		},
+		{
+			name: "istio-sidecar-injector",
+			obj: &admissionregv1.MutatingWebhookConfiguration{
+				ObjectMeta: metav1.ObjectMeta{Name: "istio-sidecar-injector"},
+			},
+			wantPass: false,
+		},
+		{
+			name: "other-webhook",
+			obj: &admissionregv1.MutatingWebhookConfiguration{
+				ObjectMeta: metav1.ObjectMeta{Name: "other-webhook"},
+			},
+			wantPass: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := filter(tt.obj); got != tt.wantPass {
+				t.Errorf("MutatingWebhookFilter() = %v, want %v", got, tt.wantPass)
+			}
+		})
+	}
+}
+
+func TestConfigMapFilter(t *testing.T) {
+	filter := ConfigMapFilter()
+	tests := []struct {
+		name     string
+		obj      *corev1.ConfigMap
+		wantPass bool
+	}{
+		{
+			name: "istio-system and istio-sidecar-injector",
+			obj: &corev1.ConfigMap{
+				ObjectMeta: metav1.ObjectMeta{Namespace: "istio-system", Name: "istio-sidecar-injector"},
+			},
+			wantPass: true,
+		},
+		{
+			name: "istio-system and istio-sidecar-injector-canary",
+			obj: &corev1.ConfigMap{
+				ObjectMeta: metav1.ObjectMeta{Namespace: "istio-system", Name: "istio-sidecar-injector-canary"},
+			},
+			wantPass: true,
+		},
+		{
+			name: "wrong namespace",
+			obj: &corev1.ConfigMap{
+				ObjectMeta: metav1.ObjectMeta{Namespace: "default", Name: "istio-sidecar-injector"},
+			},
+			wantPass: false,
+		},
+		{
+			name: "wrong name prefix",
+			obj: &corev1.ConfigMap{
+				ObjectMeta: metav1.ObjectMeta{Namespace: "istio-system", Name: "other-config"},
+			},
+			wantPass: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := filter(tt.obj); got != tt.wantPass {
+				t.Errorf("ConfigMapFilter() = %v, want %v", got, tt.wantPass)
+			}
+		})
+	}
+}
+
+func TestConfigMapReconciler_lastModifiedChanged(t *testing.T) {
+	scheme := runtime.NewScheme()
+	_ = corev1.AddToScheme(scheme)
+	fakeClient := fake.NewClientBuilder().WithScheme(scheme).Build()
+
+	r := NewConfigMapReconciler(fakeClient, scheme, false, true, 0, 0, nil, nil)
+
+	t1 := time.Date(2024, 1, 15, 10, 0, 0, 0, time.UTC)
+	t2 := time.Date(2024, 1, 15, 11, 0, 0, 0, time.UTC)
+
+	// First call: no cache, should report changed
+	if !r.lastModifiedChanged("default", t1) {
+		t.Error("lastModifiedChanged: want true (no cache)")
+	}
+
+	// Set cache
+	r.setCache("istio-system/cm", "default", t1)
+
+	// Same timestamp: should report not changed
+	if r.lastModifiedChanged("default", t1) {
+		t.Error("lastModifiedChanged: want false (same timestamp)")
+	}
+
+	// Different timestamp: should report changed
+	if !r.lastModifiedChanged("default", t2) {
+		t.Error("lastModifiedChanged: want true (timestamp changed)")
+	}
+}
+
+func TestConfigMapReconciler_getCacheCopy(t *testing.T) {
+	scheme := runtime.NewScheme()
+	_ = corev1.AddToScheme(scheme)
+	fakeClient := fake.NewClientBuilder().WithScheme(scheme).Build()
+
+	t1 := time.Date(2024, 1, 15, 10, 0, 0, 0, time.UTC)
+	r := NewConfigMapReconciler(fakeClient, scheme, false, true, 0, 0, nil, nil)
+	r.setCache("istio-system/cm", "default", t1)
+
+	copy := r.getCacheCopy()
+	if len(copy) != 1 {
+		t.Fatalf("getCacheCopy: want 1 entry, got %d", len(copy))
+	}
+	if !copy["default"].Equal(t1) {
+		t.Errorf("getCacheCopy: want %v, got %v", t1, copy["default"])
+	}
+}
+
+func TestConfigMapReconciler_clearCacheByConfigMap(t *testing.T) {
+	scheme := runtime.NewScheme()
+	_ = corev1.AddToScheme(scheme)
+	fakeClient := fake.NewClientBuilder().WithScheme(scheme).Build()
+
+	r := NewConfigMapReconciler(fakeClient, scheme, false, true, 0, 0, nil, nil)
+	r.setCache("istio-system/cm", "default", time.Now())
+
+	r.clearCacheByConfigMap("istio-system/cm")
+	copy := r.getCacheCopy()
+	if len(copy) != 0 {
+		t.Errorf("clearCacheByConfigMap: want empty cache, got %d entries", len(copy))
+	}
+}
+
+func TestConfigMapReconciler_annotateWorkloadsWithDelay(t *testing.T) {
+	scheme := runtime.NewScheme()
+	_ = corev1.AddToScheme(scheme)
+	fakeClient := fake.NewClientBuilder().WithScheme(scheme).Build()
+
+	t.Run("no delay annotates all workloads", func(t *testing.T) {
+		ann := &countingAnnotator{}
+		r := &ConfigMapReconciler{
+			Client:       fakeClient,
+			annotator:    ann,
+			dryRun:       false,
+			restartDelay: 0,
+		}
+		workloads := []podscanner.WorkloadRef{
+			{NamespacedName: types.NamespacedName{Namespace: "ns1", Name: "dep1"}, Kind: "Deployment"},
+			{NamespacedName: types.NamespacedName{Namespace: "ns2", Name: "dep2"}, Kind: "Deployment"},
+		}
+		r.annotateWorkloadsWithDelay(context.Background(), workloads)
+		if got := ann.getCount(); got != 2 {
+			t.Errorf("annotateWorkloadsWithDelay(restartDelay=0): annotator called %d times, want 2", got)
+		}
+	})
+
+	t.Run("delay between workloads", func(t *testing.T) {
+		ann := &countingAnnotator{}
+		r := &ConfigMapReconciler{
+			Client:       fakeClient,
+			annotator:    ann,
+			dryRun:       false,
+			restartDelay: 1 * time.Second,
+		}
+		workloads := []podscanner.WorkloadRef{
+			{NamespacedName: types.NamespacedName{Namespace: "ns1", Name: "dep1"}, Kind: "Deployment"},
+			{NamespacedName: types.NamespacedName{Namespace: "ns2", Name: "dep2"}, Kind: "Deployment"},
+		}
+		start := time.Now()
+		r.annotateWorkloadsWithDelay(context.Background(), workloads)
+		elapsed := time.Since(start)
+		if got := ann.getCount(); got != 2 {
+			t.Errorf("annotateWorkloadsWithDelay: annotator called %d times, want 2", got)
+		}
+		if elapsed < 1*time.Second {
+			t.Errorf("annotateWorkloadsWithDelay: elapsed %v, want >= 5ms (delay between workloads)", elapsed)
+		}
+	})
+
+	t.Run("context cancel during delay exits early", func(t *testing.T) {
+		ann := &countingAnnotator{}
+		r := &ConfigMapReconciler{
+			Client:       fakeClient,
+			annotator:    ann,
+			dryRun:       false,
+			restartDelay: 1 * time.Second,
+		}
+		workloads := []podscanner.WorkloadRef{
+			{NamespacedName: types.NamespacedName{Namespace: "ns1", Name: "dep1"}, Kind: "Deployment"},
+			{NamespacedName: types.NamespacedName{Namespace: "ns2", Name: "dep2"}, Kind: "Deployment"},
+		}
+		ctx, cancel := context.WithCancel(context.Background())
+		done := make(chan struct{})
+		go func() {
+			r.annotateWorkloadsWithDelay(ctx, workloads)
+			close(done)
+		}()
+		time.Sleep(10 * time.Millisecond)
+		cancel()
+		<-done
+		if got := ann.getCount(); got != 1 {
+			t.Errorf("annotateWorkloadsWithDelay (cancelled during delay): annotator called %d times, want 1 (second workload skipped)", got)
+		}
+	})
+
+	t.Run("dry-run does not annotate", func(t *testing.T) {
+		ann := &countingAnnotator{}
+		r := &ConfigMapReconciler{
+			Client:       fakeClient,
+			annotator:    ann,
+			dryRun:       true,
+			restartDelay: 0,
+		}
+		workloads := []podscanner.WorkloadRef{
+			{NamespacedName: types.NamespacedName{Namespace: "ns1", Name: "dep1"}, Kind: "Deployment"},
+		}
+		r.annotateWorkloadsWithDelay(context.Background(), workloads)
+		if got := ann.getCount(); got != 0 {
+			t.Errorf("annotateWorkloadsWithDelay (dry-run): annotator called %d times, want 0", got)
+		}
+	})
+}
