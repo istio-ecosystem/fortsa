137201457318eeb28cf9c85cb3fc401829857668
Steve Scaffidi <sscaffidi@cloudera.com>
2026-02-24 14:55:31 -0500

    delete ALL the things
    Signed-off-by: Steve Scaffidi <sscaffidi@cloudera.com>

--- internal/common/common.go
+++
@@ -1,9 +0,0 @@
-package common
-
-const (
-	// k8s object label for istio revision
-	IstioRevLabel = "istio.io/rev"
-
-	// k8s object label for istio revision tag
-	IstioTagLabel = "istio.io/tag"
-)
