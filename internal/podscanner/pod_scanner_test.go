b658705c6cd2935de9e6c1f1f7e5a9d31549c2b7
Steve Scaffidi <sscaffidi@cloudera.com>
2026-02-24 15:06:10 -0500

    copied files from fortsa2 and modified the package for imports
    Signed-off-by: Steve Scaffidi <sscaffidi@cloudera.com>

--- internal/podscanner/pod_scanner_test.go
+++ internal/podscanner/pod_scanner_test.go
@@ -1,19 +1,3 @@
-/*
-Copyright 2026.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package podscanner
 
 import (
@@ -711,7 +695,7 @@ func TestScanOutdatedPods_tagResolution(t *testing.T) {
 		if len(workloads) != 1 {
 			t.Errorf("want 1 workload, got %d", len(workloads))
 		}
-		if webhook.calledWithRevision != "1-20" { //nolint:goconst
+		if webhook.calledWithRevision != "1-20" {
 			t.Errorf("webhook called with revision %q, want 1-20 (resolved from tag canary)", webhook.calledWithRevision)
 		}
 	})
@@ -842,7 +826,7 @@ func TestScanOutdatedPods_istioInjectionLabel(t *testing.T) {
 		if len(workloads) != 1 {
 			t.Errorf("want 1 workload (istio-injection=enabled), got %d", len(workloads))
 		}
-		if webhook.calledWithRevision != "default" { //nolint:goconst
+		if webhook.calledWithRevision != "default" {
 			t.Errorf("webhook called with revision %q, want default (from istio-injection=enabled)", webhook.calledWithRevision)
 		}
 	})
